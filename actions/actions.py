c4ba1a12fb2bd3f2b31e9b39e3f87030863b9d6d
Satyabrata Sahoo <satya.sahoo2@tcs.com>
2022-03-27 19:21:25 +0530

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,131 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from datetime import datetime
+from .config import news_api as news
+from .config import userName, customer
+
+
+class ActionGreet(Action):
+
+    def name(self) -> Text:
+        return "action_greet"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        now = datetime.now()
+
+        if now.hour < 12:
+            day = 'Morning'
+        elif now.hour < 18:
+            day = 'Afternoon'
+        else:
+            day = 'Evening'
+
+        dispatcher.utter_message(text=f"Hello {userName}! Good {day}")
+
+        return []
+
+
+# class ActionIdentity(Action):
+
+#     def name(self) -> Text:
+#         return "action_iamabot"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(text=f"I am NewsBot 4.0 - Customized for {customer}")
+#         return []
+
+
+class ActionHeadlines(Action):
+
+    def name(self) -> Text:
+        return 'action_headlines'
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        news_items = news.get_top_headlines()
+        if news_items.get('totalResults', 0) > 0:
+            articles = news_items['articles']
+            for article in articles[:3]:
+                title = article['title']
+                description = article['description']
+                url = article['url']
+                image_url = article['urlToImage']
+                source = article['source']['name']
+
+                message = f'{title}\n[see more]({url})'
+                dispatcher.utter_message(text=message, image=image_url)
+        else:
+            dispatcher.utter_message(text='No recent news on the topic.')
+
+        return []
+
+
+class ActionSpecificNews(Action):
+
+    def name(self) -> Text:
+        return 'action_specific_news'
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        print(tracker)
+        q = (tracker.latest_message['entities'] or [{}])[0].get('value')
+        news_items = news.get_everything(q=q, sort_by='relevancy')
+        if news_items.get('totalResults', 0) > 0:
+            if q:
+              dispatcher.utter_message(text=f'Here are some news about *{q}*')
+            article = news_items['articles'][0]
+
+            title = article['title']
+            description = article['description']
+            url = article['url']
+            image_url = article['urlToImage']
+            source = article['source']['name']
+
+            message = f'{title}\n[see more]({url})'
+            dispatcher.utter_message(text=message, image=image_url)
+        else:
+            dispatcher.utter_message(text=f'No recent news on the topic *{q or ""}*.')
+
+        return []
---
+++ actions/config.py
@@ -0,0 +1,7 @@
+from newsapi import NewsApiClient
+
+__api_key = 'f70b9ed1b31e4f558c539e2a83c5b373'
+news_api = NewsApiClient(api_key=__api_key)
+
+userName = 'Satya'
+customer = 'TCS'
